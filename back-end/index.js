6e47f9e3c94af9fd72bde98e89a3aa81382b81e8
HB78 <78313137+HB78@users.noreply.github.com>
2022-06-03 14:11:21 +0200

    mise en place de mongoose

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 
+//on importe mongoose dans le fichier principal index.js depuis le dossier models et dbConfig.js
+//si on importe pas une paquet dans le fichier principal il ne fonctionnera pas
+const mongoose = require('./models/dbConfig');
+
 //on donne par defaut le port 3000 si, il est occupé il recherchera un port libre
 let port = process.env.PORT || 3000;
 
---
+++ back-end/models/dbConfig.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+//pour se connecter à la base de données, mongoose fait le lien entre l'api et mongoDB
+mongoose.connect(
+    "mongodb://localhost:27017/node-api",
+    {useNewUrlParser : true, useUnifiedTopology: true},
+    (err) => {
+        if(!err) console.log("vous etes connecté à mongodb");
+        else console.log("la connexion à mongodb a échouée" + err);
+    }
+)
